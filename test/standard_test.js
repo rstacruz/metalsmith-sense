084896be8463a1650222b20b8254b5dff4b820a4
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-08-14 10:49:11 +0800

    Update standard tests

--- test/standard_test.js
+++ test/standard_test.js
@@ -1,9 +1,14 @@
 describe('coding style', function () {
   this.timeout(5000)
-  it('conforms to standard', require('mocha-standard').files([
-    'lib/**/*.js',
+
+  it('lib conforms to standard', require('mocha-standard').files([
+    '*.js',
+    'lib/**/*.js'
+  ]))
+
+  it('tests conform to standard', require('mocha-standard').files([
     'test/**/*.js'
   ], {
-    global: ['describe', 'it', 'before', 'beforeEach', 'after', 'afterEach']
+    global: ['describe', 'it', 'before', 'beforeEach', 'after', 'afterEach', 'xdescribe', 'xit']
   }))
 })
